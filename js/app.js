91dd0d6056c651e87010e2f938265d4687ee2911
jasanchez32 <jennifersanchez784@gmail.com>
2026-04-02 21:20:04 -0500

    Cambio de colores y estructura.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,14 @@
 const input = document.querySelector('input');
 const button = document.querySelector('button');
 const tasksList = document.getElementById('tasks');
+const boxList = document.querySelector('fieldset');
+
+boxList.style.display = 'none';
 
 button.addEventListener('click', () => {
     const taskText = input.value;
     if (taskText.trim() !== '') {
+        boxList.style.display = 'block';
         const li = document.createElement('li');
         li.textContent = taskText;
         tasksList.appendChild(li);
@@ -16,6 +20,7 @@ function addTask() {
     const text = input.value;
     if(text.trim() === '') return;
 
+    boxList.style.display = 'block';
     const li = document.createElement('li');
     li.textContent = text;
     tasksList.appendChild(li);
