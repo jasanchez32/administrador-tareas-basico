c1ecf396f6086e2a887ae6c381ed28349fa8549e
jasanchez32 <jennifersanchez784@gmail.com>
2026-04-04 17:14:48 -0500

    Generar localstorage

--- js/app.js
+++ js/app.js
@@ -1,28 +1,60 @@
 const input = document.querySelector('input');
 const button = document.querySelector('button');
-const tasksList = document.getElementById('tasks');
+const list = document.getElementById('tasks');
 const boxList = document.querySelector('fieldset');
+let tasks = [];
+const savedTasks = localStorage.getItem('tasks');
 
-boxList.style.display = 'none';
+if (savedTasks) {
+    tasks = JSON.parse(savedTasks);
+    renderTasks();
+} else {
+    boxList.style.display = 'none';
+}
 
 button.addEventListener('click', () => {
-    const taskText = input.value;
-    if (taskText.trim() !== '') {
-        boxList.style.display = 'block';
-        const li = document.createElement('li');
-        li.textContent = taskText;
-        tasksList.appendChild(li);
-        input.value = '';
-    }
+    addTask();
 });
 
+
+/** * Adds a new task to the task list.
+ * This function retrieves the text from the input field, checks if it's not empty, and then adds it to the tasks array.
+ * After adding the task, it calls the renderTasks function to update the displayed list of tasks and clears the input field.   
+ * * @function
+ * @returns {void}
+ */
 function addTask() {
     const text = input.value;
     if(text.trim() === '') return;
 
-    boxList.style.display = 'block';
-    const li = document.createElement('li');
-    li.textContent = text;
-    tasksList.appendChild(li);
+    tasks.push(text);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    renderTasks();
     input.value = '';
 }
+
+/** * Renders the list of tasks on the webpage.
+ * This function clears the current list of tasks displayed on the page and then iterates through the tasks array, creating a new list item for each task and appending it to the list. Finally, it makes sure the task list is visible by setting the display style of the boxList to 'block'.
+ * @function
+ * @returns {void}
+ */
+function renderTasks() {
+    list.innerHTML = '';
+    tasks.forEach(task => {
+        const listItem = document.createElement('li');
+        listItem.textContent = task;
+        list.appendChild(listItem);
+    });
+    boxList.style.display = 'block';
+}
+
+/** * Deletes the last task from the task list.
+ * This function removes the last task from the tasks array using the pop method and then calls the renderTasks function to update the displayed list of tasks.
+ * @function
+ * @returns {void}
+ */
+function deleteTask() {
+    tasks.pop();
+    renderTasks();
+}
+
